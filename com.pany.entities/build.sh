f371f6c781b1191055945c03f7b0e3a0d3729a69
rickaas <rtlindeman@gmail.com>
2013-05-17 20:49:20 +0200

    make build configurable

--- com.pany.entities/build.sh
+++ com.pany.entities/build.sh
@@ -1,3 +1,33 @@
-#! /bin/sh
+#!/bin/bash
 
-ANT_OPTS="-Xss8m -Xmx1024m -server -XX:+UseParallelGC -XX:MaxPermSize=256m" ant -f build.main.external.xml "$@"
+
+#SCRIPT=$(readlink -f $0)
+#SCRIPTPATH=$(dirname $SCRIPT)
+
+echo "$@"
+
+if [ "ANT_BUILD" == "$1" ] ; then
+	echo ant build
+	shift
+	ANT_OPTS="-Xss8m -Xmx1024m -server -XX:+UseParallelGC -XX:MaxPermSize=256m" ant -f build.main.external.xml "$@"
+	#exit
+fi
+
+#./build.sh PACK_STR -I trans/ -I . -i trans/companyentities.str -o ce.aterm
+
+#utils="/home/rlindeman/Documents/TU/strategoxt/git-stuff/dsldi/dist/dist-libdsldi/debug"
+# release
+utils="/home/rlindeman/Documents/TU/strategoxt/git-stuff/dsldi/dist/dist-libdsldi/release"
+
+
+
+# ./build.sh -i ... -o -I ... 
+if [ "PACK_STR" == "$1" ] ; then
+	echo pack stratego
+	shift
+	#CLASSPATH="-cp $SCRIPTPATH/../share/strategoxt/strategoxt.jar"
+	#JAVA_OPTS="-Xss8m"
+	#java $JAVA_OPTS $CLASSPATH run org.strategoxt.strc.pack-stratego-io $@
+	$utils/run.sh "MAIN_CLASS" org.strategoxt.strc.pack-stratego-io $@
+fi
+
